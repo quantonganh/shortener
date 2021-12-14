13d22ef30b5ef2cbb0ccd926c5e63e4b76541587
Quan Tong <quantonganh@gmail.com>
2021-12-15 05:50:41 +0700

    add frontend using Svelte

---
+++ ui/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	let longURL
+	let shortURL
+
+	async function doShorten() {
+        const res = await fetch('/create-short-url', {
+            method: 'POST',
+	        body: JSON.stringify({
+		        'long_url': longURL
+	        })
+        })
+
+		const json = await res.json()
+		shortURL = json.short_url
+	}
+</script>
+
+<form on:submit|preventDefault={doShorten}>
+	<input required placeholder="Long URL" type="url" bind:value={longURL}>
+	<button>Shorten</button>
+</form>
+
+<a href="{shortURL}">{shortURL}</a>
